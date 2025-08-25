6356362d74d058e616a902802fd0705ef5e12057
Aleksandar Georgiev <549190@student.saxion.nl>
2025-08-25 22:50:37 +0200

    didnt save

--- Unity Project/Assets/Scripts PP2/asdfgasdfgasdfg/Inventory.cs
+++ Unity Project/Assets/Scripts PP2/asdfgasdfgasdfg/Inventory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Inventory : MonoBehaviour
 {
@@ -61,6 +62,7 @@ public abstract class Inventory : MonoBehaviour
 
             if (startingItem.item == null)
             {
+                Image image = GetComponentInChildren<Image>();
                 continue;
             }
 
